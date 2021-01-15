619daf41386cd95fdd51052e4c5c62a8bb15a7d7
ala8343 <ala8343@gmail.com>
2021-01-16 00:46:19 +0900

    2nd submission - 20210116

---
+++ Programmers/스택큐_주식가격.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+def solution(prices):
+    answer = []
+    
+    que_prices = deque(prices)
+    
+    while que_prices:
+        price = que_prices.popleft()
+        up_time = 0
+        for n in que_prices:
+            up_time += 1
+            if price > n:
+                break
+        answer.append(up_time)
+        
+    return answer
